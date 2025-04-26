7aa8e033593e7ebc07b39f9f4dee30700f8e5cd0
kenta9502 <tatata9502@gmail.com>
2025-04-27 00:23:38 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_product, only: [:show, :edit, :update]
+  before_action :ensure_correct_user_for_edit, only: [:edit, :update]
+
   def index
     @products = Product.order(created_at: :desc)
   end
@@ -11,7 +14,7 @@ class ItemsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to root_path @product, notice: 'Product was successfully created.'
+      redirect_to root_path, notice: '商品を出品しました。'
     else
       render :new, status: :unprocessable_entity
     end
@@ -21,10 +24,32 @@ class ItemsController < ApplicationController
     @product = Product.includes(:user).find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product), notice: '商品情報を更新しました。'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:image, :name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
                                     :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def ensure_correct_user_for_edit
+    return if @product.user_id == current_user.id
+
+    redirect_to root_path, alert: '出品者以外のユーザーは編集できません。'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'  # 'items#index' はコントローラーとアクションに応じて変更します
-  resources :items, only: [:new, :create, :show] # 'new'アクションへのルートを追加
-  resources :products
+  resources :items, only: [:new, :create, :show,:edit] # 'new'アクションへのルートを追加
+  resources :products, controller: 'items'
 
 end
