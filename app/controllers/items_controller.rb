3b3b623dd25990d0762ae6f491dfb55a87d7b00a
kenta9502 <tatata9502@gmail.com>
2025-04-27 17:40:20 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_product, only: [:show, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :ensure_correct_user_for_edit, only: [:edit, :update]
+  before_action :ensure_correct_user_for_destroy, only: [:destroy]
 
   def index
     @products = Product.order(created_at: :desc)
@@ -34,6 +35,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @product.destroy
+      redirect_to root_path, notice: '商品を削除しました。'
+    else
+      redirect_to item_path(@product), alert: '商品の削除に失敗しました。'
+    end
+  end
+
   private
 
   def product_params
@@ -49,5 +58,11 @@ class ItemsController < ApplicationController
     return if @product.user_id == current_user.id
 
     redirect_to root_path, alert: '出品者以外のユーザーは編集できません。'
+  end
+
+  def ensure_correct_user_for_destroy
+    return if @product.user_id == current_user.id
+
+    redirect_to root_path, alert: '出品者以外のユーザーは削除できません。'
   end
 end
